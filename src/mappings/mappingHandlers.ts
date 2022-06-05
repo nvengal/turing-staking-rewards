734f76f4f97badd6a23844d0fc6e9a1b5996d008
Nikhil Vengal <ngvengal@gmail.com>
2022-06-04 23:03:13 -0700

    Index staking reward events

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,33 +1,17 @@
-import {SubstrateExtrinsic,SubstrateEvent,SubstrateBlock} from "@subql/types";
-import {StarterEntity} from "../types";
-import {Balance} from "@polkadot/types/interfaces";
+import { SubstrateEvent } from "@subql/types";
+import { StakingReward } from "../types";
 
+const TUR = 10000000000.0;
 
-export async function handleBlock(block: SubstrateBlock): Promise<void> {
-    //Create a new starterEntity with ID using block hash
-    let record = new StarterEntity(block.block.header.hash.toString());
-    //Record block number
-    record.field1 = block.block.header.number.toNumber();
-    await record.save();
+export async function handleParachainStakingReward(substrateEvent: SubstrateEvent): Promise<void> {
+  const { idx, block, event } = substrateEvent;
+  const blockHeight = block.block.header.number;
+  const data = event.data.toJSON();
+  const attrs = {
+    id: `staking-reward-${blockHeight}-${idx}`,
+    block: blockHeight.toBigInt(),
+    account: data[0],
+    amount: data[1]/TUR
+  };
+  await StakingReward.create(attrs).save();
 }
-
-export async function handleEvent(event: SubstrateEvent): Promise<void> {
-    const {event: {data: [account, balance]}} = event;
-    //Retrieve the record by its ID
-    const record = await StarterEntity.get(event.block.block.header.hash.toString());
-    record.field2 = account.toString();
-    //Big integer type Balance of a transfer event
-    record.field3 = (balance as Balance).toBigInt();
-    await record.save();
-}
-
-export async function handleCall(extrinsic: SubstrateExtrinsic): Promise<void> {
-    const record = await StarterEntity.get(extrinsic.block.block.header.hash.toString());
-    //Date type timestamp
-    record.field4 = extrinsic.block.timestamp;
-    //Boolean tyep
-    record.field5 = true;
-    await record.save();
-}
-
-
